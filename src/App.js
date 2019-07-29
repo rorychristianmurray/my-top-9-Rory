3bc0cd3ecdf5ecd793052075acf84e41ddb0a589
Rory Murray <rory.christian.murray@gmail.com>
2019-07-29 15:49:42 -0400

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// Dependencies
+import React from 'react';
+
+// Objects
+import DataList from './components/DataList';
+
+// Stylings
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>React Redux Template</div>
+      <DataList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Data.js
@@ -0,0 +1,18 @@
+// Dependencies
+import React from "react";
+import { connect } from "react-redux";
+
+// Objects
+
+// Styles
+
+const Data = props => {
+  console.log("Data props", props);
+  return (
+    <div>
+      <div>Data rendering</div>
+    </div>
+  );
+};
+
+export default Data;
