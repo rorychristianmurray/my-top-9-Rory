d316b13ff7fc56b3fa2b9299941862e360b9cad5
Rory Murray <rory.christian.murray@gmail.com>
2019-07-30 14:21:40 -0400

    Create Login component, initialize state

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Login = props => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+
+  return <div />;
+};
+
+export default Login;
